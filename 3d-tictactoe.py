f5fc5e00b6baf147c2a757a3f1637f0812d55442
Plrrb <plrrbsctipt@gmail.com>
2021-03-17 17:12:54 -0700

    done

--- 3d-tictactoe.py
+++ 3d-tictactoe.py
@@ -1,39 +1,46 @@
-empty = " "
+empty = ' '
 
 cube = [
     [
-        [" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "],
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
     ],
     [
-        [" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "],
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
     ],
     [
-        [" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "],
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
     ],
 ]
 
 players = [
-    {"symbol": "X", "score": 0},
-    {"symbol": "O", "score": 0},
+    {
+        'symbol': 'X',
+        'score': 0
+    },
+    {
+        'symbol': 'O',
+        'score': 0
+    },
 ]
 
 
 def draw():
     for row in range(3):
         for layer in cube:
-            print(f" {layer[row][0]} | {layer[row][1]} | {layer[row][2]} ", end=" ")
-        print("")
+            print(f' {layer[row][0]} | {layer[row][1]} | {layer[row][2]} ',
+                  end=' ')
+        print('')
         if row != 2:
-            print("---|---|--- ---|---|--- ---|---|---")
+            print('---|---|--- ---|---|--- ---|---|---')
 
     for player in players:
-        print(player["symbol"], "Score:", player["score"])
+        print(player['symbol'], 'Score:', player['score'])
 
 
 def is_layer_full(layer):
@@ -51,7 +58,7 @@ def input_to_int(text):
             inp = int(inp)
             break
         except:
-            print("not a number")
+            print('not a number')
 
     return inp
 
@@ -59,21 +66,21 @@ def input_to_int(text):
 def get_input(player):
     # get input for layer
     while True:
-        layer = input_to_int(player + " choose a layer: ") - 1
+        layer = input_to_int(player + ' choose a layer: ') - 1
         if layer >= 3 or layer < 0:
-            print("number out of bounds")
+            print('number out of bounds')
             continue
 
         if not is_layer_full(layer):
             break
         else:
-            print("that layer is full")
+            print('that layer is full')
 
     # get input for place
     while True:
-        place = input_to_int(player + " choose a place: ") - 1
+        place = input_to_int(player + ' choose a place: ') - 1
         if not 0 <= place <= 9:
-            print("number out of bounds")
+            print('number out of bounds')
             continue
 
         row = place // 3
@@ -83,11 +90,12 @@ def get_input(player):
             cube[layer][row][index] = player
             break
         else:
-            print("that place is taken")
+            print('that place is taken')
 
 
 def check_win(player):
     wins = 0
+
     # all in a row
     for layer in cube:
         for row in layer:
@@ -108,42 +116,31 @@ def check_win(player):
 
     # one layer diagonal
     for layer in cube:
-        if (
-            player == layer[0][0] == layer[1][1] == layer[2][2]
-            or player == layer[0][2] == layer[1][1] == layer[2][0]
-        ):
+        if (player == layer[0][0] == layer[1][1] == layer[2][2]
+                or player == layer[0][2] == layer[1][1] == layer[2][0]):
             wins += 1
 
     # multi layer diagonal
     for i in range(3):
-        if (
-            player == cube[0][0][i] == cube[1][1][i] == cube[2][2][i]
-            or player == cube[0][2][i] == cube[1][1][i] == cube[2][0][i]
-        ):
+        if (player == cube[0][0][i] == cube[1][1][i] == cube[2][2][i]
+                or player == cube[0][2][i] == cube[1][1][i] == cube[2][0][i]):
             wins += 1
 
     # horizontal diagonal
     for i in range(3):
-        if (
-            player == cube[0][i][0] == cube[1][i][1] == cube[2][i][2]
-            or player == cube[2][i][0] == cube[1][i][1] == cube[0][i][2]
-        ):
+        if (player == cube[0][i][0] == cube[1][i][1] == cube[2][i][2]
+                or player == cube[2][i][0] == cube[1][i][1] == cube[0][i][2]):
             wins += 1
 
     # thru opposite corners of the cube
     # top left
-    if (
-        player == cube[0][0][0] == cube[1][1][1] == cube[2][2][2]
-        or
-        # top right
-        player == cube[0][0][2] == cube[1][1][1] == cube[2][2][0]
-        or
-        # bottom left
-        player == cube[0][2][0] == cube[1][1][1] == cube[2][0][2]
-        or
-        # bottom right
-        player == cube[0][2][2] == cube[1][1][1] == cube[2][0][0]
-    ):
+    if (player == cube[0][0][0] == cube[1][1][1] == cube[2][2][2] or
+            # top right
+            player == cube[0][0][2] == cube[1][1][1] == cube[2][2][0] or
+            # bottom left
+            player == cube[0][2][0] == cube[1][1][1] == cube[2][0][2] or
+            # bottom right
+            player == cube[0][2][2] == cube[1][1][1] == cube[2][0][0]):
         wins += 1
 
     return wins
@@ -154,10 +151,12 @@ game_over = False
 while not game_over:
     for player in players:
         draw()
-        get_input(player["symbol"])
 
-        player["score"] = check_win(player["symbol"])
-        if player["score"] >= 3:
-            print(player["symbol"], "wins")
+        get_input(player['symbol'])
+
+        player['score'] = check_win(player['symbol'])
+
+        if player['score'] >= 3:
+            print(player['symbol'], 'wins')
             draw()
             game_over = True
